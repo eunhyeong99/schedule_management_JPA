a7bb00c9d7ca9f66aa35841492d8ce1783e1ce1a
Jo Eun Hyeong <whdmsgud8@naver.com>
2024-08-23 12:29:40 +0900

    init commit

---
+++ src/test/java/com/sparta/schedule_management_jpa/ScheduleManagementJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedule_management_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
